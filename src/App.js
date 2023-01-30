1bf7c8dc8d65e735883c4b1470e5235a89796032
Patrick Schubert 2021 <patrickschubert@PatricksMBP2021.fritz.box>
2023-01-31 00:42:18 +0100

    first commit: Created initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import Homepage from "./Homepage.js";
+import Mint from "./Mint.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <header>
+              <nav>
+                  <div className="logo">
+                      <h1> <a href=""> Grandpa's Chat </a></h1>
+                  </div>
+
+                  <ul>
+                      <li><a href="./"> Home </a> </li>
+                      <li className="nav-cta"><a href="/Mint"> Mint </a></li>
+                  </ul>
+              </nav>
+          </header>
+
+
+          <Router>
+              <Routes>
+                  <Route path="/" element={<Homepage />} />
+                  <Route path="/Mint" element={<Mint />} />
+
+
+
+              </Routes>
+
+          </Router>
     </div>
   );
 }
---
+++ src/Mint.js
@@ -0,0 +1,12 @@
+//Mint.js
+import "./Mint.css";
+function Mint(props) {
+
+    return (
+        <div>
+            <button class="mint-button"><a href="#">Mint</a></button>
+        </div>
+    );
+}
+
+export default Mint;
