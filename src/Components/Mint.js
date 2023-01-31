d9cf1eb2eb19e4e16045bfa63a60e0fa5075175c
Patrick <patrick@me.com>
2023-01-31 06:09:38 +0100

    refactor: Create Mint function to the button Mint

--- src/Components/Mint.js
+++ src/Components/Mint.js
@@ -1,9 +1,32 @@
 import "./Mint.css";
+import { ethers } from "ethers";
+import ABI from "./ABI.json"
+
 function Mint(props) {
+
+    const mint = async () => {
+        console.log("Running Mint:");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+            "0x027778474953c38aEf46ADE7b08357C88773f4af",
+            ABI,
+            signer
+        );
+        const price = await contract.price();
+        console.log("show price of NFT in wai: " + price);
+        console.log(price);
+
+        const tryToMint = await contract.safeMint({value: price});
+
+        console.log("Running tryToMint: " + tryToMint);
+    }
 
     return (
         <div>
-            <button class="mint-button"><a href="#">Mint</a></button>
+            <button onClick={mint} class="mint-button">Mint
+                {/* <a onClick={mint} href="#">Mint</a> */}
+            </button>
         </div>
 
 
